e52955fdd991d044a601b8699b5492d1fdec66f7
Kristian Hartikainen <kristian@statwing.com>
2015-11-07 22:37:28 +0200

    New questions stuff

--- static/questions.js
+++ static/questions.js
@@ -1,35 +1,23 @@
 var NUM_QUESTIONS = 3;
 var ANSWERS = {
-    "q1": ["nature", "urban"],
+    "q1": ["urban", "nature"],
     "q2": ["adventure", "chill"],
     "q3": ["warm", "cold"]
 };
 
-var createTitle = function(answers) {
-    return answers[0].capitalize() +
-        " or " +
-        answers[1].capitalize() +
-        "?";
+var hideInfoBar = function() {
+    $(".info-bar").addClass("hidden");
 }
 
-var renderQuestion = function(questionNumber) {
-    var answers = ANSWERS["q" + (questionNumber+1)];
-    var opt1 = $(".question button.option-1");
-    var opt2 = $(".question button.option-2");
-    var title = createTitle(answers);
-
-    $(".question p").text(title);
-    opt1.text(answers[0]);
-    opt1.data("answer", answers[0]);
-    opt2.text(answers[1]);
-    opt2.data("answer", answers[1]);
-
+var displayQuestion = function(questionNumber) {
+    $(".question").removeClass("active");
+    $(".question-" + (questionNumber+1)).addClass("active");
 }
 
 var startQuestions = function() {
     var question = 0;
     var answers = [];
-    renderQuestion(question);
+    displayQuestion(question);
 
     var answerQuestion = function(evt) {
         var target = $(evt.currentTarget);
@@ -37,16 +25,20 @@ var startQuestions = function() {
 
         answers.push(answer);
 
-        ++question;
         if (question < NUM_QUESTIONS) {
-            renderQuestion(question);
+            ++question;
+            if (question > 0) {
+                hideInfoBar();
+            }
+            displayQuestion(question);
         } else {
             queryStr = answers.join("+");
             window.location.replace("/experiences?query=" + queryStr);
+            return;
         }
     }
 
-    $(".answer button").on("click", answerQuestion);
+    $(".question .option").on("click", answerQuestion);
 }
 
 $(function() {
