f48336c41d202544db4c4bf6fe7bf9d97a8ec8f2
Mylly Keisari <keisari@kebabpyora.com>
2015-11-08 09:39:12 +0000

    add db configuration

--- application.py
+++ application.py
@@ -1,12 +1,38 @@
-from mongoengine import *
 from flask import Flask, render_template, request
 from flights import fetch_flights, fetch_nearest_airports
+from flask.ext.sqlalchemy import SQLAlchemy
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import Column, Integer, String, BigInteger
+
+import os
+
+db_url = ''
 
 app = Flask(__name__, static_folder='static', static_url_path='/static')
 app.debug = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost:5432/junction'
+app.config['SQLALCHEMY_DATABASE_URI'] = db_url
+db = SQLAlchemy(app)
 
-connect('junction')
+Base = declarative_base()
+
+class Experience(Base):
+    __tablename__ = 'experiences'
+    key = db.Column('key_column',db.BigInteger, primary_key=True)
+    experience = db.Column('experience', db.String) 
+    place = db.Column('place', db.String)
+    country = db.Column('country', db.String)
+    image = db.Column('image', db.String)
+    description = db.Column('description', db.String)
+    nature_urban = db.Column('nature_urban', db.String)
+    adventure_chill = db.Column('adventure_chill', db.String)
+    airbnb = db.Column('airbnb', db.String)
+    airbnb_price = db.Column('airbnb_price', db.Integer)
+    experience_link = db.Column('experience_link', db.String)
+    experience_price = db.Column('experience_price', db.Integer)
+
+for instance in db.session.query(Experience):
+    print instance
 
 @app.route('/', methods=['GET'])
 def questions():
